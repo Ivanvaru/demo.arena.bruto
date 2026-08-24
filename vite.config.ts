5685b1ab25859a66b8f4aca5fff2ea4aa2dec22e
Ivanvaru <ivan.valentin.98@gmail.com>
2026-08-24 20:11:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "56b4a2db-71fe-4a40-ad3e-abf5d04bc824";
 
 const { d1, r2 } = hostingConfig;
 
@@ -19,7 +19,7 @@ const localBindingConfig = {
     ? [
         {
           binding: d1,
-          database_name: "site-creator-d1",
+          database_name: "arena-brutos-demo-db",
           database_id: SITE_CREATOR_PLACEHOLDER_DATABASE_ID,
         },
       ]
